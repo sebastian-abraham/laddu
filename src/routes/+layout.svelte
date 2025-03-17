fb52b2f71afba07ad1376a873766c25aaaf9cf56
Sebastian <sebastianabraham2006@gmail.com>
2025-03-18 01:23:15 +0530

    feat: intilized sveltekit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+  import { slide } from "svelte/transition";
+  import { page } from "$app/stores";
+  import { quintOut } from "svelte/easing";
+</script>
+
+{#key $page.url.pathname}
+  <div>
+    <slot />
+  </div>
+{/key}
